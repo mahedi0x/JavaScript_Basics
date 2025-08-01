710ceecd00be0f799a7e2ec897cd5722c9ba861a
Mahedi0x <mahedih.cse@gmail.com>
2025-08-02 00:08:20 +0600

    Starting BasicJs

---
+++ BasicJs/keyword.js
@@ -0,0 +1,23 @@
+// Variable Declarations
+var, let, const
+
+
+// Control Flow & Looping
+if, else, switch, case, default, for, do, while, break, continue
+
+
+// Functions & Classes
+function, return, class, constructor, extends, super, static, this, new
+
+
+// Asynchronous JavaScript 
+async, await, yield
+
+
+// Error Handling
+try, catch, finally, throw
+
+
+// Other Operators & Keywords
+delete, in, instanceof, typeof, void, with, debugger
+
---
+++ BasicJs/variable.js
@@ -0,0 +1,22 @@
+// --------------Numeric---------------
+let age = 25;
+let number = 50;
+
+let height = 5.6;
+let price = 19.99;
+
+// --------------String-------------------
+let name = "John Doe";
+let greeting = 'Hello World';
+
+// ---------------Boolean-------------------
+let isLoggedIn = true;
+let isAdult = false;
+
+
+// ----------------Variable Typeof --------------
+console.log(typeof(name));
+
+let type = typeof(isAdult);
+console.log(type);
+
